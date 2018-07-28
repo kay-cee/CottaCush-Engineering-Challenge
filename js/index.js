c2bba47e3e3a7eafd1ae7113c1d77a2a1985d14e
kay-cee <anikenechi.c@gmail.com>
2018-07-28 00:10:23 -0400

    backup plan

--- js/index.js
+++ js/index.js
@@ -3,20 +3,26 @@
 //this algorithms assume that the the distances are in multiples of 1000
 
 function maxBanana(x,y,z) {
-    var x = bananaLeft(x,y);
+
+    //the algorithm divides the journey into three having an arbitrary points A and B in-between
+    x = bananaLeft(x,y);
+    console.log("The amount of bananas left after the first trip set: ");
     bananaLeft(x,y);
     var x1 = bananaLeft(x,y);
+    console.log("The amount of bananas left after the second trip set: ");
     bananaLeft(x1,y);
     var x2 = bananaLeft(x1,y);
-    x2 = x2*z
-    $("#answer").val(x2)
+    x2 = x2*z;
+    $("#answer").val(x2);
+    alert("The max amount of bananas left is: " + x2);
 }
 
 function bananaLeft(x,y) {
     // y is the total distance and since for efficency the trip has to be divided into 3
     var y2 = y/3;
     var a = tripNo(x)*y2;
-    var left = x-a;
+    var left;
+    left = x - a;
     return left;
 }
 
@@ -38,12 +44,11 @@ function noOfBananas() {
 
 function noOfCamels() {
     var z  = $("#noc").val();
-    // alert("camels = "+z)
     return z;
 }
 
 function distanceToMarket() {
-    var y = $("#dtm").val()
+    var y = $("#dtm").val();
     if (y<1000){
         $("#distance-to-market").val("invalid no of camels, no less than 1000")
     }
@@ -56,7 +61,7 @@ function distanceToMarket() {
 }
 
 function bananaEatenPerKm() {
-    var k = $("#BperKm").val()
+    var k = $("#BperKm").val();
     if (y<1000){
         $("#bananas-eaten").val("invalid no of camels, no less than 1000")
         $("#BperKm").empty();
@@ -70,20 +75,14 @@ function bananaEatenPerKm() {
 }
 
 $("#submit").click(function () {
-    // alert("Shit it loaded");
+
+    console.log("button click recorded");
     var x = noOfBananas();
     var y = distanceToMarket();
     var k = bananaEatenPerKm();
     var z = noOfCamels();
+    console.log("Values fetch success");
 
     maxBanana(x,y,z);
-    $("#answer").val(u).show();
-    alert("The max bananas is: ")
 
-});
-
-$(document).ready(function () {
-    // $("small").hide();
-    $(".answer").hide();
-    // $("#nob-bananas-help").val("Enter a value greater than or equal to 3000");
 });
